4632b66c7bb10554e41cb6d72132394031f7e06b
pckuo <b04201003@gmail.com>
2024-08-01 10:42:26 +0800

    add payment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from orders.models import Order
 
-# Create your models here.
+class Payment(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment')
+    payment_status = models.CharField(max_length=50)
+    transaction_id = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=0)
+
+    def __str__(self):
+        return f'Payment for order {self.order.id}'
